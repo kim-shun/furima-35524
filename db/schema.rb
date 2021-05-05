7ea1ab04186aae3eca5232d42ebf387831b60b95
kim-shun <kimura1417080@gmail.com>
2021-05-05 15:39:37 +0900

    生年月日の登録

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_051912) do
+ActiveRecord::Schema.define(version: 2021_05_05_063520) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_05_05_051912) do
     t.string "first_name"
     t.string "last_name_kana"
     t.string "first_name_kana"
-    t.integer "birth_date"
+    t.date "birth_date"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
