0f49edd1b97e8fb115abcdba462fabbec66be9d5
kim-shun <kimura1417080@gmail.com>
2021-05-06 17:50:46 +0900

    コメントの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -90,12 +90,23 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordが半角英数字混同でなければ登録できない' do
-        @user.password = '12345'
-        @user.password_confirmation = '12345'
+      it 'passwordが英語のみでは登録できない' do
+        @user.password = 'testtest'
+        @user.password_confirmation = 'testtest'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)',
-                                                      'Password には英字と数字の両方を含めて設定してください')
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      end
+      it 'passwordが数字のみでは登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      end
+      it 'passwordが全角では登録できない' do
+        @user.password = 'あいうえおか'
+        @user.password_confirmation = 'あいうえおか'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
       end
       it 'last_nameとfirst_nameが全角でなければ登録できない' do
         @user.last_name = 'test'
