c5bbde2d236cad65c40bc634b71dd72453f32c3d
kim-shun <kimura1417080@gmail.com>
2021-05-08 10:47:25 +0900

    Activehashデータの変更

--- app/models/scheduled_delivery.rb
+++ app/models/scheduled_delivery.rb
@@ -1,9 +1,9 @@
 class ScheduledDelivery < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '1日〜3日' },
-    { id: 3, name: '4日〜6日' },
-    { id: 4, name: '7日以上' }
+    { id: 2, name: '1日〜2日で発送' },
+    { id: 3, name: '2日〜3日で発送' },
+    { id: 4, name: '4日〜7日で発送' },
   ]
 
   include ActiveHash::Associations
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :item do
     name {Faker::Lorem.word}
     info {Faker::Lorem.sentence}
-    price {Faker::Number.number} #もっと適切な値があるかも
-    category_id {Faker::Number.between(from: 2, to: 6)}
-    sales_status_id {Faker::Number.between(from: 2, to: 4)}
-    shipping_fee_status_id {Faker::Number.between(from: 2, to: 4)}
+    price {Faker::Number.between(from: 300, to: 9999999)}
+    category_id {Faker::Number.between(from: 2, to: 11)}
+    sales_status_id {Faker::Number.between(from: 2, to: 7)}
+    shipping_fee_status_id {Faker::Number.between(from: 2, to: 3)}
     prefecture_id {Faker::Number.between(from: 2, to: 48)}
     scheduled_delivery_id {Faker::Number.between(from: 2, to: 4)}
     association :user #紐づいていないかも
