7a35b72a8e6b8077000e0854158d275404da0d48
kim-shun <kimura1417080@gmail.com>
2021-05-08 16:45:16 +0900

    rubocopの実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
   has_one_attached :image
-  
+
   with_options presence: true do
     validates :name
     validates :info
@@ -20,9 +20,9 @@ class Item < ApplicationRecord
     validates :scheduled_delivery_id
   end
 
-  validates :price, format: {with: /\A[0-9]+\z/, message: '半角数字を使用してください' }
-  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  
+  validates :price, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :sales_status_id
@@ -30,5 +30,4 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :scheduled_delivery_id
   end
-  
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -35,82 +35,82 @@ RSpec.describe Item, type: :model do
       it 'カテゴリーが空では登録できない' do
         @item.category_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+        expect(@item.errors.full_messages).to include("Category can't be blank", 'Category is not a number')
       end
       it '商品の状態が空では登録できない' do
         @item.sales_status_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Sales status can't be blank", "Sales status is not a number")
+        expect(@item.errors.full_messages).to include("Sales status can't be blank", 'Sales status is not a number')
       end
       it '配送料の負担が空では登録できない' do
         @item.shipping_fee_status_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping fee status can't be blank", "Shipping fee status is not a number")
+        expect(@item.errors.full_messages).to include("Shipping fee status can't be blank", 'Shipping fee status is not a number')
       end
       it '発送元の地域が空では登録できない' do
         @item.prefecture_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture can't be blank", "Prefecture is not a number")
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank", 'Prefecture is not a number')
       end
       it '発送までの日数が空では登録できない' do
         @item.scheduled_delivery_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank", "Scheduled delivery is not a number")
+        expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank", 'Scheduled delivery is not a number')
       end
       it '商品価格が空では登録できない' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank", "Price is not a number")
+        expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is not a number')
       end
       it 'categoryidが1では登録できない' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category must be other than 1")
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
       it 'sales_status_idが1では登録できない' do
         @item.sales_status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Sales status must be other than 1")
+        expect(@item.errors.full_messages).to include('Sales status must be other than 1')
       end
       it 'shipping_fee_status_idが1では登録できない' do
         @item.shipping_fee_status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping fee status must be other than 1")
+        expect(@item.errors.full_messages).to include('Shipping fee status must be other than 1')
       end
       it 'prefecture_idが1では登録できない' do
         @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+        expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
       it 'scheduled_delivery_idが1では登録できない' do
         @item.scheduled_delivery_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Scheduled delivery must be other than 1")
+        expect(@item.errors.full_messages).to include('Scheduled delivery must be other than 1')
       end
       it '商品価格が299円以下では登録できない' do
         @item.price = 200
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it '商品価格が10,000,000円以上では登録できない' do
-        @item.price = 10000100
+        @item.price = 10_000_100
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
       it '商品価格が半角英語では登録できない' do
         @item.price = 'test'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it '商品価格が全角文字では登録できない' do
         @item.price = 'あいうえお'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it '商品価格が全角数字では登録できない' do
         @item.price = '１００００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it 'ユーザーが紐付いていなければ登録できない' do
         @item.user = nil
