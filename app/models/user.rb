93541e0efd4e55b8089b45e48f47fc4e3e6a36a7
kim-shun <kimura1417080@gmail.com>
2021-05-09 11:27:11 +0900

    条件分岐の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :items
 
   with_options presence: true do
-    validates :nick_name
+    validates :nickname
     validates :birth_date
 
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
