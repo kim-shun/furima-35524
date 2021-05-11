41352799b24350c3ea5c6951d7a0d1142cf0fe6f
kim-shun <kimura1417080@gmail.com>
2021-05-11 20:32:33 +0900

    バリデーションの変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,11 +9,11 @@ class OrderAddress
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :address
-    validates :phone_number, numericality: {only_integer: true} 
+    validates :phone_number, numericality: {only_integer: true } 
     validates :token, presence: true
   end
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
-
+    validates :phone_number, length: {minimum: 11, maximum: 11}
 
   def save
     
