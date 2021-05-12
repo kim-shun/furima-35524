29bdf46d9be78d7280a66916f6d0c23b093784bd
kim-shun <kimura1417080@gmail.com>
2021-05-12 11:15:22 +0900

    ログインの有無による遷移の違い

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,7 @@ class OrdersController < ApplicationController
     @order = Order.new
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
+    move_to_index
   end
 
   def new
@@ -28,7 +29,19 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
-  
+
+  def move_to_index
+    if current_user.id == @item.user.id && @item.order.present?
+      redirect_to root_path
+    end
+    if current_user.id == @item.user.id && @item.order.nil?
+      redirect_to root_path
+    end
+    if current_user.id != @item.user.id && @item.order.present?
+      redirect_to root_path
+    end
+  end
+
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
