d21daa771533d679b940c5726f67fed62206538c
kim-shun <kimura1417080@gmail.com>
2021-05-12 12:03:05 +0900

    コントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,14 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_order, only: [:index, :create]
   
   def index
     @order = Order.new
     @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
     move_to_index
   end
 
-  def new
-    @order_address = OrderAddress.new
-  end
-
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -28,6 +23,10 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_order
+    @item = Item.find(params[:item_id])
   end
 
   def move_to_index
