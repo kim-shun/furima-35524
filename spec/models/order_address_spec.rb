a1069dfb7d0f4b0a16d6b3ff5e021f728d479754
kim-shun <kimura1417080@gmail.com>
2021-05-12 15:33:57 +0900

    ダミー画像の読み込みの制限

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe OrderAddress, type: :model do
   describe '購入情報情報の保存' do
     before do
       user = FactoryBot.create(:user)
-      item = FactoryBot.build(:item)
+      item = FactoryBot.create(:item)
       @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
     end
 
