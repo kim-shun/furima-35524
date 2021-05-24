d0407349dfc4bed591d95b5a3e977d97f50d4471
kim-shun <kimura1417080@gmail.com>
2021-05-24 11:59:33 +0900

    カードのテストコード

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,4 +1,6 @@
 FactoryBot.define do
   factory :card do
+    card_token { 'tok_abcdefghijk00000000000000000' }
+    customer_token { 'tok_abcdefghijk00000000000000000' }
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    @card = FactoryBot.build(:card, user_id: user.id)
+  end
+
+  context '内容に問題ない場合' do
+    it "card_tokenがあれば保存ができること" do
+      expect(@card).to be_valid
+    end
+  end
+
+  context '内容に問題がある場合' do
+    it "card_tokenが空では登録できないこと" do
+      @card.card_token = nil
+      @card.valid?
+      expect(@card.errors.full_messages).to include("Card token can't be blank")
+    end
+    it 'ユーザーが紐付いていなければ登録できない' do
+      @card.user = nil
+      @card.valid?
+      expect(@card.errors.full_messages).to include('User must exist')
+    end
+  end
 end
