60e5a89c6455360f965811fc5961348d59346929
kim-shun <kimura1417080@gmail.com>
2021-06-03 12:22:27 +0900

    アソシエーションとdeviseの再設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
+  
   get 'cards/new'
   get 'comments/new'
   get 'items/search'
-  devise_for :users
+  
   root to: 'items#index'
   resources :items do
     resources :orders, only: [:index, :create]
