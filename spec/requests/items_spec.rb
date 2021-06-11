1febfd2e6604c6af6a6382afc8f6ef5ce60d0321
kim-shun <kimura1417080@gmail.com>
2021-06-12 08:46:09 +0900

    items_controllerのテストコードの途中

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -10,5 +10,35 @@ describe ItemsController, type: :request do
       get root_path
       expect(response.status).to eq 200
     end
+
+    it 'indexアクションにリクエストするとレスポンスに出品済みの商品名が存在する' do 
+      get root_path
+      expect(response.body).to include(@item.name)
+    end
+
+    #画像のテストコードがまだ
+
+    it 'indexアクションにリクエストするとレスポンスに商品検索フォームが存在する' do 
+      get root_path
+      expect(response.body).to include('検索')
+    end
   end
+
+  describe 'GET #show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      get item_path(@item)
+      expect(response.status).to eq 200
+    end
+    it 'showアクションにリクエストするとレスポンスに出品済みの商品名が存在する' do
+      get item_path(@item)
+      expect(response.body).to include(@item.name)
+    end
+    
+    it 'showアクションにリクエストするとレスポンスに出品者名が存在する' do
+      get item_path(@item)
+      expect(response.body).to include(@item.user.nickname)
+    end
+
+    #カテゴリー以降とコメントもある
+  end 
 end
